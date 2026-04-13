a1934d3cb5d418dc92d8a51351444062dafa298a
Miriam Romero <miriromero15@gmail.com>
2026-04-13 13:05:00 +0200

    JS modificated and add function operate

--- javascript.js
+++ javascript.js
@@ -7,29 +7,82 @@ const screen = document.querySelector(".screen")
 
 
 //operation variables
-let num1 = Number("");
+let num1 = "";
 let operator = "";
-let num2 = Number("");
+let num2 = "";
 
 //listener
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         if (button.classList.contains("equal") || button.classList.contains("delete")) return;
+        if(screen.textContent.length > 8) return;
         screen.textContent += button.textContent;
+
+        if (button.classList.contains("operator")) {
+            operator += button.textContent; 
+            
+            /*if(operator.length == 2) {
+
+            }*/
+        }
+
+
         if(operator.length === 0) {
             num1 += button.textContent;
         }else if (operator.length === 1) {
-            num2 += button.textContent;
+            if(!button.classList.contains("operator")) {
+               num2 += button.textContent; 
+            }
+            
         }
     });
 });
 
-//guardar operators en operator
 
-//limit case
+//equal listener
+equal.addEventListener("click", () =>{
+    if(num1 === "" || operator === "" || num2 === "") return;
+    else operate();
+});
+
+//operate function
+function operate() {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    if (operator == "+") screen.textContent = add(num1, num2);
+    else if(operator == "-") screen.textContent = subtract(num1, num2);
+    else if(operator == "*") screen.textContent = multiply(num1, num2);
+    else if(operator == "/") screen.textContent = divide(num1, num2);
+}
+
+//delete button
+deletebtn.addEventListener("click", () => {
+    screen.textContent = "";
+    num1 = "";
+    operator = "";
+    num2 = "";
+})
+
 
 
 //add function
+function add(a, b) {
+ return a + b;
+}   
 
+//substract function 
+function subtract(a, b) {
+    return a - b;
+}
+
+//multiply function
+function multiply(a, b) {
+    return a * b;
+}
+
+//divide function
+function divide(a, b) {
+    return a / b;
+}
 
 
