d2bbe92aac8c30cdaacff918931a52ef6e932669
Miriam Romero <miriromero15@gmail.com>
2026-04-13 18:30:47 +0200

    Finish main functions

--- javascript.js
+++ javascript.js
@@ -10,20 +10,31 @@ const screen = document.querySelector(".screen")
 let num1 = "";
 let operator = "";
 let num2 = "";
+let resultShown = false;
 
 //listener
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         if (button.classList.contains("equal") || button.classList.contains("delete")) return;
         if(screen.textContent.length > 8) return;
+
+        if (resultShown == true) {
+             screen.textContent = "";
+            num1 = "";
+            operator = "";
+            num2 = "";
+            resultShown = false;
+        }
+
         screen.textContent += button.textContent;
 
         if (button.classList.contains("operator")) {
-            operator += button.textContent; 
             
-            /*if(operator.length == 2) {
-
-            }*/
+            if(num2 !== "") { 
+                num1 = parseFloat(operate().toFixed(2));
+                num2= "";
+            }
+            operator = button.textContent; 
         }
 
 
@@ -42,17 +53,18 @@ buttons.forEach(button => {
 //equal listener
 equal.addEventListener("click", () =>{
     if(num1 === "" || operator === "" || num2 === "") return;
-    else operate();
+    else screen.textContent = parseFloat(operate().toFixed(2));
+    resultShown = true;
 });
 
 //operate function
 function operate() {
     num1 = Number(num1);
     num2 = Number(num2);
-    if (operator == "+") screen.textContent = add(num1, num2);
-    else if(operator == "-") screen.textContent = subtract(num1, num2);
-    else if(operator == "*") screen.textContent = multiply(num1, num2);
-    else if(operator == "/") screen.textContent = divide(num1, num2);
+    if (operator == "+") return add(num1, num2);
+    else if(operator == "-") return subtract(num1, num2);
+    else if(operator == "*") return multiply(num1, num2);
+    else if(operator == "/") return divide(num1, num2);
 }
 
 //delete button
@@ -82,7 +94,12 @@ function multiply(a, b) {
 
 //divide function
 function divide(a, b) {
-    return a / b;
+    if(b == 0) {
+        return "You can't divide with zero!";
+    } else {
+       return a / b; 
+    }
+    
 }
 
 
